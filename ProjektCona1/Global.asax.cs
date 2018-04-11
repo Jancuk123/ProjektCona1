358927b5340c70789ad4c24f72e6a4dca18a9cb4
študent <student@c.scng.si>
2018-04-11 11:56:37 +0200

    Sprememba na global.asx.cs

--- ProjektCona1/Global.asax.cs
+++ ProjektCona1/Global.asax.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-
+//komentar123456
 namespace ProjektCona1
 {
     public class MvcApplication : System.Web.HttpApplication
